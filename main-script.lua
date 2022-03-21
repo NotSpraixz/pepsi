6683f2e49257f91dfc85ff58bc4b429f0a0a5526
TotallyAFakeSpraixz <72309547+NotSpraixz@users.noreply.github.com>
2022-03-21 17:38:48 +0700

    Update main-script.lua

--- main-script.lua
+++ main-script.lua
@@ -1,3 +1,5 @@
+-- Support Discord Server: http://discord.gg/VzYTJ7Y
 shared.autoload = false -- change to profile name if you want to autoload (afking/autojoin). eg. shared.autoload = "profile"
-
-loadstring(game:GetObjects("rbxassetid://4384103988")[0X1].Source)("Pepsi Swarm")
+shared.skipenter = false -- Set to true if you don't want to keep pressing Enter when entering values in the gui
+shared.autoreconnect = false -- Set to true if you want Pepsi Swarm to automatically reconnect you to a new server
+loadstring(game:GetObjects("rbxassetid://4384103988")[0x1].Source)("Pepsi Swarm")
